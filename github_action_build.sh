6aa06b7e9929fb4670e0c3a7f2ffc3ada571ff5d
Jung-Sang Ahn <jung-sang@couchbase.com>
2020-12-15 15:23:23 -0800

    update pr

--- github_action_build.sh
+++ github_action_build.sh
@@ -4,15 +4,17 @@ set -ex
 rm -rf build
 mkdir build
 cd build
+
+echo "head branch: ${GITHUB_HEAD_REF}"
+echo "base branch: ${GITHUB_BASE_REF}"
+echo "branch: ${GITHUB_REF}"
+
 if [ -z "${GITHUB_HEAD_REF}" ]; then
-    echo "branch: ${GITHUB_REF}"
     cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=1 -DENABLE_RAFT_STATS=1 ../
     make -j2 raft_cov
     bash <(curl -s https://codecov.io/bash) -f raft_cov.info.cleaned
 else
     echo "pull request sha: ${GITHUB_SHA}"
-    echo "head branch: ${GITHUB_HEAD_REF}"
-    echo "base branch: ${GITHUB_BASE_REF}"
     cmake -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1 -DENABLE_RAFT_STATS=1 ../
     make -j2
     echo "PR test"
